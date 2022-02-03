5d377eb5fde699a55d64a33d7a8b9d87fe2b9c66
william_castro <wicastro98@gmail.com>
2022-02-02 20:23:41 -0500

    button-up-scrollbar-added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <h1>Rick And Morty (Challenge Code)</h1>
     <ListCharacters />
+    <a class="gotopbtn" href="#"></a>
   </div>
 </template>
 
@@ -9,9 +10,18 @@
 import ListCharacters from './components/ListCharacters.vue'
 
 export default {
+  data () {
+    return {
+    }
+  },
+
   components: {
     ListCharacters
-  }
+  },
+
+  methods: {
+  },
+  
 }
 </script>
 
@@ -20,7 +30,23 @@ export default {
   box-sizing: border-box;
   margin: 1.5%;
   padding: 0;
+  scrollbar-width: none;
+  scrollbar-color: #d87213 #7f7a7a;
 }
+
+*::-webkit-scrollbar {
+  width: 16px;
+}
+
+*::-webkit-scrollbar-track {
+  background: #7f7a7a;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: #d87213;
+  border-radius: 10px;
+  border: 3px none #7f7a7a;
+  }
 
 body {
   background-color: #24282F;
@@ -39,4 +65,30 @@ h1 {
 img {
   width: 100%;
 }
+
+.gotopbtn{
+  background: url("https://img.icons8.com/ios-filled/80/000000/circled-up.png") no-repeat;
+  position: fixed;
+  width: 100px;
+  height: 100px;
+  top: 50%;
+  right: -50px;
+
+  text-decoration: none;
+  text-align: center;
+  line-height: 50px;
+  color: white;
+  font-size: 22px;
+  animation-name: example;
+  animation-duration: 2s;
+}
+
+@keyframes example {
+  0%   {right:-50px; top:47%;}
+  25%  {right:-50px; top:53%;}
+  50%  {right:-50px; top:47%;}
+  75%  {right:-50px; top:53%;}
+  100% {right:-50px; top:50%;}
+}
+
 </style>
